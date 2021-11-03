b039009d4b0f4ad73b8058d6aa76fe426c2172f8
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2021-11-03 13:31:25 +0200

    Separate routing, complete part 4.2

--- part4/blogilista/app.js
+++ part4/blogilista/app.js
@@ -1,8 +1,8 @@
 const config = require('./utils/config')
-const Blog = require('./models/blog')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
 
 mongoose.connect(config.MONGODB_URI)
@@ -10,22 +10,6 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
