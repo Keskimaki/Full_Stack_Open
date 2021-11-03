be397b4e4cebb8b2e33cd824f5cb353b0d1e6a91
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2021-11-03 15:04:46 +0200

    Part 4.5

--- part4/blogilista/utils/list_helper.js
+++ part4/blogilista/utils/list_helper.js
@@ -1,10 +1,17 @@
 const dummy = blogs => 1
 
-const totalLikes = blogs => {
-  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+const totalLikes = blogs => blogs.reduce((sum, blog) => sum + blog.likes, 0)
+
+const favoriteBlog = blogs => {
+  const blogLikes = blogs.map(blog => blog.likes)
+  const index = blogLikes.indexOf(Math.max(...blogLikes))
+  return blogs[index]
+    ? blogs[index]
+    : null
 }
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
