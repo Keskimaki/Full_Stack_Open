6a49524201b7b80e257bd6b34adbd4db18f5afb9
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2021-11-06 16:33:26 +0200

    Part 5.7

--- part5/blogilista/src/components/Blog.js
+++ part5/blogilista/src/components/Blog.js
@@ -1,8 +1,37 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+const Blog = ({blog}) => {
+  const [details, setDetails] = useState(false)
+
+  const hideWhenVisible = { display: details ? 'none': '' }
+  const showWhenVisible = {
+    display: details ? '': 'none',
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleDetails = () => {
+    setDetails(!details)
+  }
+
+  return (
+    <div>
+      <div style={hideWhenVisible} onClick={toggleDetails}>
+        <>{blog.title} {blog.author} </>
+        <button onClick={toggleDetails}>view</button>
+      </div>  
+      <div style={showWhenVisible} onClick={toggleDetails}>
+        <>{blog.title} {blog.author} </>
+        <button onClick={toggleDetails}>hide</button> <br />
+        {blog.url} <br />
+        {blog.likes} <button>like</button> <br />
+        {blog.user.name ? blog.user.name : blog.user.username}
+      </div>
+    </div>
+  )
+}
+
 
 export default Blog
