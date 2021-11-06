5ba55e327b51546402c01aa14057c0ae249c8221
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2021-11-06 17:47:51 +0200

    Part 5.11

--- part5/blogilista/src/components/Logout.js
+++ part5/blogilista/src/components/Logout.js
@@ -1,9 +1,16 @@
-//import react from "react"
+import React from "react"
+import PropTypes from 'prop-types'
+
 const Logout = ({ user, handleLogout}) => (
   <p>
     <>{user.name ? user.name : user.username} logged in </>
     <button onClick={handleLogout}>logout</button>
   </p>
 )
+
+Logout.propTypes = {
+  user: PropTypes.object.isRequired,
+  handleLogout: PropTypes.func.isRequired
+}
 
 export default Logout
