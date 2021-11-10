27fd3b1e4ad6b1dbd23697ca4511e817d784bddb
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2021-11-10 21:41:08 +0200

    Part 6.15

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -4,7 +4,6 @@ import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
 import { initializeAnecdotes } from './reducers/anecdoteReducer'
-import anecdoteService from './services/anecdotes'
 import { useDispatch } from 'react-redux'
 
 const App = () => {
@@ -12,8 +11,7 @@ const App = () => {
   const dispatch = useDispatch()
 
   useEffect(() => {
-    anecdoteService
-      .getAll().then(anecdotes => dispatch(initializeAnecdotes(anecdotes))) 
+    dispatch(initializeAnecdotes())
   }, [dispatch])
 
   return (
