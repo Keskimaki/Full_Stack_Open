9892a7bc0d0bb2a650090831d55cbd266165d7d8
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2021-11-11 12:48:51 +0200

    Part 7.5

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -72,23 +72,27 @@ const CreateNew = (props) => {
     })
   }
 
+  const handleReset = () => {
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          content
-          <input {...content} />
+          content <input {...content} />
         </div>
         <div>
-          author
-          <input {...author} />
+          author <input {...author} />
         </div>
         <div>
-          url for more info
-          <input {...info} />
+          url for more info <input {...info} />
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
+        <button type="reset" onClick={handleReset}>reset</button>
       </form>
     </div>
   )
