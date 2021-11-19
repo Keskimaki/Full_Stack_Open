fa5fcd1776bc8758aac6221cb6a6423f52e324be
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2021-11-19 14:08:37 +0200

    Part 9.15

--- part9/react-typescript/src/components/Content.tsx
+++ part9/react-typescript/src/components/Content.tsx
@@ -1,16 +1,47 @@
 import React from "react";
+import { CoursePart } from "../types";
 
-interface Course {
-  name: string;
-  exerciseCount: number;
-}
-
-const Content = ({ courseParts }: { courseParts: Array<Course> }) => (
+const Content = ({ courseParts }: { courseParts: CoursePart[] }) => (
   <div>
     {courseParts.map(coursePart => 
-      <p key={coursePart.name}>{coursePart.name} {coursePart.exerciseCount}</p>
+      <Part key={coursePart.name} coursePart={coursePart} />
     )}
   </div>
 );
+
+const Part = ( {coursePart}: { coursePart: CoursePart} ) => {
+  switch (coursePart.type) {
+    case "normal":
+      return (
+        <p>
+          <strong>{coursePart.name} {coursePart.exerciseCount}</strong> <br />
+          <em>{coursePart.description}</em>
+        </p>
+      )
+    case "groupProject":
+      return (
+        <p>
+          <strong>{coursePart.name} {coursePart.exerciseCount}</strong> <br />
+          project exercises {coursePart.groupProjectCount}
+        </p>
+      )
+    case "submission":
+      return (
+        <p>
+          <strong>{coursePart.name} {coursePart.exerciseCount}</strong> <br />
+          <em>{coursePart.description}</em> <br />
+          submit to {coursePart.exerciseSubmissionLink}
+        </p>
+      )
+    case "special":
+      return (
+        <p>
+          <strong>{coursePart.name} {coursePart.exerciseCount}</strong> <br />
+          <em>{coursePart.description}</em> <br />
+          required skills: {coursePart.requirements.join(", ")}
+        </p>
+      )
+  }
+};
 
 export default Content;
