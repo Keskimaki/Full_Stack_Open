bed4be488df1b4a82f823b9a814f3362434731d2
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2021-11-19 12:02:34 +0200

    Part 9.13

---
+++ part9/patientor-backend/build/src/utils.js
@@ -0,0 +1,42 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const types_1 = require("./types");
+const isString = (text) => {
+    return typeof text === 'string' || text instanceof String;
+};
+const parseString = (name) => {
+    if (!name || !isString(name)) {
+        throw new Error('Incorrect or missing name');
+    }
+    return name;
+};
+const isDate = (date) => {
+    return Boolean(Date.parse(date));
+};
+const parseDate = (date) => {
+    if (!date || !isString(date) || !isDate(date)) {
+        throw new Error('Incorrect or missing date: ' + date);
+    }
+    return date;
+};
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const isGender = (param) => {
+    return Object.values(types_1.Gender).includes(param);
+};
+const parseGender = (gender) => {
+    if (!gender || !isGender(gender)) {
+        throw new Error('Incorrect or missing gender: ' + gender);
+    }
+    return gender;
+};
+const toNewPatient = ({ name, dateOfBirth, ssn, gender, occupation }) => {
+    const newPatient = {
+        name: parseString(name),
+        dateOfBirth: parseDate(dateOfBirth),
+        ssn: parseString(ssn),
+        gender: parseGender(gender),
+        occupation: parseString(occupation)
+    };
+    return newPatient;
+};
+exports.default = toNewPatient;
--- part9/patientor-backend/src/routes/patients.ts
+++ part9/patientor-backend/src/routes/patients.ts
@@ -1,6 +1,6 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express from 'express';
 import patientService from '../services/patientService';
+import toNewPatient from '../utils';
 
 const patientRouter = express.Router();
 
@@ -9,15 +9,17 @@ patientRouter.get('/', (_req, res) => {
 });
 
 patientRouter.post('/', (req, res) => {
-  const { name, dateOfBirth, ssn, gender, occupation } = req.body;
-  const newPatient = patientService.addPatient({
-    name,
-    dateOfBirth,
-    ssn,
-    gender,
-    occupation
-  });
-  res.json(newPatient);
+  try {
+    const newPatient = toNewPatient(req.body);
+    const addedNewPatient = patientService.addPatient(newPatient);
+    res.json(addedNewPatient);
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong.';
+    if(error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
 });
 
 export default patientRouter;
